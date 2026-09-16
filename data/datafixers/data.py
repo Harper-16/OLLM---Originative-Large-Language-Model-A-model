3cb8d72b276c0f4d6b8d9dd893a3a9c7a2740cf7
player6 <192975739+Harper-16@users.noreply.github.com>
2026-09-16 14:25:58 +0300

    Update dataset path in data.py

--- data/datafixers/data.py
+++ data/datafixers/data.py
@@ -1,6 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("yousefsaeedian/databricksdatabricks-dolly-15k")
+path = kagglehub.dataset_download("The/path/to/data")
 
 print("Path to dataset files:", path)
